d58aded9f3722b7ec9fbdaad56fe46c3bcabf261
Andrea <andreaverace98@hotmail.it>
2022-05-23 14:23:00 +0200

    esercizio

---
+++ src/main/java/it/manytomanyjpamaven/exception/UtenteConRuoliAssociatiException.java
@@ -0,0 +1,13 @@
+package it.manytomanyjpamaven.exception;
+
+public class UtenteConRuoliAssociatiException extends Exception {
+
+	private String message;
+	
+	public UtenteConRuoliAssociatiException(String message) {
+		super(message);
+	}
+	
+	
+	
+}
